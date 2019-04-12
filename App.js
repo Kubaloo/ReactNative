7aad2eff2b251375c0e0ae67afbcc55018cacb7e
Jakub <jakub.suski000@gmail.com>
2019-04-12 15:43:34 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from 'react';
+import Main from './app/Components/Main';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
